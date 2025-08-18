c31ed951373aa59d349f1ea3fa3a7b1e4a678022
T Sushmanth <tsushmanth@gmail.com>
2025-08-18 16:31:55 +0530

    First Intialize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Users from "./users/pages/Users";
+import NewPlace from "./places/pages/NewPlaces";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Home (Users page) */}
+        <Route path="/" element={<Users />} />
+
+        {/* New Place page */}
+        <Route path="/newplace" element={<NewPlace />} />
+
+        {/* Catch-all redirect */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
