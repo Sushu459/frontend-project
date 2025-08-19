9c0da705edf15f06748b95839f6c4792e0277fdb
T Sushmanth <tsushmanth@gmail.com>
2025-08-19 12:40:18 +0530

    Added Users components and updated Users.js

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,5 +1,22 @@
 import React from 'react';
-const Users = ()=>{
-    return <h2>Users Works!</h2>;
+
+// Importing the UsersList component
+import UsersList from '../components/UsersList';
+
+const Users = () => {
+  // Hardcoded array of users
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Sushmanth',
+      image:
+        'https://raw.githubusercontent.com/Sushu459/my-photos/main/profile.jpg',
+      places: 3,
+    },
+  ];
+
+  // Passing USERS as props to UsersList
+  return <UsersList items={USERS} />;
 };
+
 export default Users;
