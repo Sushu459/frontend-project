4a97d3ac22b1e34f5576cf90a6d906dcf234c74a
T Sushmanth <tsushmanth@gmail.com>
2025-09-01 13:33:01 +0530

    added new components Navigatin and files

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Users from "./users/pages/Users";
 import NewPlace from "./places/pages/NewPlaces";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <BrowserRouter>
+    <MainNavigation />
+    <main>
       <Routes>
         {/* Home (Users page) */}
         <Route path="/" element={<Users />} />
@@ -16,6 +19,7 @@ const App = () => {
         {/* Catch-all redirect */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+      </main>
     </BrowserRouter>
   );
 };
---
+++ src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import Backdrop from "./Backdrop"; // optional overlay
+
+import "./MainNavigation.css";
+
+const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      {/* Backdrop when drawer is open */}
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+
+      {/* Side Drawer */}
+      <SideDrawer show={drawerIsOpen}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
+      {/* Main Header */}
+      <MainHeader>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your Places..!</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
+  );
+};
+
+export default MainNavigation;
